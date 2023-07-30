5d0c45978b3f10a3f855b606c6e5d8933ea8e1fd
rugema3 <rugema61@gmail.com>
2023-07-30 22:17:06 +0200

    I just added a function that sells the animal and update the zoo animals by removind the sold animal.

--- python_practicing/create_animal.py
+++ python_practicing/create_animal.py
@@ -32,12 +32,34 @@ def show_zoo():
                     animals in teh zoo.
 
     """
-    if len(zoo) == 0:
+    if not zoo:
         print("There have no animals in the zoo.")
     else:
         print("Animals in the zoo: ")
         for animal in zoo:
-            print(animal)
+            print(animal.name)
+
+def sell_animal(name):
+    """Define sell_animal.
+
+    args:
+    name(str):  This is the name of the animal to be sold.
+
+    Description:
+    This function will sell the animal and update the zoo list.
+    The function will delete the sold animal from the list in the zoo.
+    """
+    animal_for_sale = None
+    for animal_instance in zoo:
+        if animal_instance.name == name:
+            animal_for_sale = animal_instance
+            break
+    if animal_for_sale is not None:
+        zoo.remove(animal_for_sale)
+        print("{} has been sold.It is no longer on the list.".format(name))
+    else:
+        print("No animal in the zoo called {} ".format(name))
+
 
 if __name__ == '__main__':
     """Main function to handle user choices."""
@@ -45,16 +67,21 @@ if __name__ == '__main__':
         print("\nWhat would you like to do?")
         print("1. Create an animal")
         print("2. View animals in the zoo")
-        print("3. Exit")
+        print("3. Sell animal in the zoo.")
+        print("4. Exit")
         
-        choice = input("Enter your choice (1, 2, or 3): ")
+        choice = input("Enter your choice (1, 2, 3, or 4): ")
         
         if choice == '1':
             create_animal()
         elif choice == '2':
             show_zoo()
-
         elif choice == '3':
+            name = input("please enter the animal you want to sell: ")
+            name = name.lower()
+            sell_animal(name)
+
+        elif choice == '4':
             print("Goodbye!")
             break
         else:
