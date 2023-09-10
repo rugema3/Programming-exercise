a8dd1008fb9f1c217325b969bcc48a25a84ee212
rugema3 <rugema61@gmail.com>
2023-09-10 09:26:13 +0200

    this script contains a function that multiplies two numbers and returns the product in JS.

---
+++ javascript/functions.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/**
+ * multiply - This functions multiplies two numbers.
+ *
+ * @param {number} num1 - The number to be multiplied
+ * @param {number} num2 - The number to be multiplied
+ * @returns {number} result - The product of num1 and num2
+ */
+
+function multiply(num1, num2)
+{
+	let result = num1 * num2;
+	return result;
+}
+
+console.log(multiply(10, 3));
