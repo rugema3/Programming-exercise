562777d9d003cf24bce708ccf69263c333f97d02
rugema3 <rugema61@gmail.com>
2023-10-25 14:44:01 +0200

    I just added posting to different other discord channels

--- automations/discord.py
+++ automations/discord.py
@@ -20,7 +20,10 @@ import requests
 import config # the file that contains the headers for auntentication.
 import jokes # contains the jokes obtained from the jokes api.
 authorisation = config.authorisation
-end_point = config.url
+end_point = config.url # Url for the cohort_13 channel discord
+url_review = config.url_review # url for the cohort_13 review channel
+url_kigali = config.url_kigali # url for the Kigali channel Software engineers
+url_general = config.url_general # URL for the general Channel
 
 
 def discord_post(msg):
@@ -39,9 +42,61 @@ def discord_post(msg):
     result = requests.post(url, payload, headers=headers)
     print(result)
 
+def discord_post_review(msg):
+    """This function will post the message ona  discord channel.
+    The name of the channel is cohort_13 review. This is a discord channel
+    of ALX_Africa software engineering students.
+
+    args:
+        msg: message to be posted on the channel.
+    """
+    url = url_review
+    payload = {
+            "content" : msg
+            }
+    headers = {
+            "Authorization": authorisation
+            }
+    result = requests.post(url, payload, headers=headers)
+    print(result)
+
+def discord_post_general(msg):
+    """This function will post on the channel called general.
+    args:
+            msg : message to be posted.
+
+    """
+    url = url_general
+    
+    payload = {
+            "content" : msg
+            }
+    headers = {
+            "Authorization": authorisation
+            }
+    result = requests.post(url, payload, headers=headers)
+    print(result)
+
+
+def discord_post_kigali(msg):
+    """Post of Kigali Channel.
+    args:
+            msg:    message to be posted
+    """
+    url = url_kigali
+    payload = {
+            "content" : msg
+            }
+    headers = {
+            "Authorization": authorisation
+            }
+    result = requests.post(url, payload, headers=headers)
+    print(result)
+
+
 if __name__ == '__main__':
     msg = jokes.get_programming_joke()
     discord_post(msg)
-
-
-
+    discord_post_kigali(msg)
+    discord_post_review(msg)
+    discord_post_general(msg)
