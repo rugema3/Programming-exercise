a71f941126caecf1a8fc422129f65abadc1e0eb4
rugema3 <rugema61@gmail.com>
2024-12-29 00:06:37 +0200

    added more functions and updated the class so that I can start posting on my Linkedin

--- automations/social_media_auto.py
+++ automations/social_media_auto.py
@@ -2,6 +2,9 @@ import config
 import requests
 import jokes
 import os
+from today_in_history import get_historical_events
+import datetime
+from discord import discord_post_general
 
 access_token = config.LINKEDIN_ACCESS_TOKEN
 user_id = config.USER_ID
@@ -65,14 +68,64 @@ class SocialMediaAutomator:
             return "Post was successful."
         else:
             return f"Post failed with status code: {response.status_code}"
+        
+def format_for_linkedin(events, character_limit=2800):
+    """
+    Formats historical events for a LinkedIn post, ensuring it stays within the character limit.
+    Args:
+        events (list): List of events as returned by the API.
+        character_limit (int): Maximum number of characters allowed for the LinkedIn post.
+    Returns:
+        str: A formatted string of events.
+    """
+    # Get today's date and months
+    today = datetime.date.today()
+    day = today.day
+    month_name = today.strftime("%B")
+    post_content = f"🌟 Historical Events for Today ({month_name} {day} in history):\n\n"
+    char_count = len(post_content)
+
+    for event in events:
+        year = event.get("year", "Unknown year")
+        description = event.get("text", "")
+        entry = f"📜 {year}: {description}\n"
+        
+        if char_count + len(entry) <= character_limit:
+            post_content += entry
+            char_count += len(entry)
+        else:
+            post_content += "\n.#History #OnThisDay #WorldEvents #HistoricalEvents #Knowledge #Learning #Facts #Education #Discover #Explore #CulturalHeritage #GlobalHistory"
+            break
+
+    return post_content
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     social_media_automator = SocialMediaAutomator()
-    
-    post_content = jokes.get_programming_joke()
-    
-    result = social_media_automator.create_post(post_content)
-    
-    print(result)
 
+    today = datetime.date.today()
+    month = today.month 
+    day = today.day  
+    historical_data = get_historical_events(month, day)
+    
+    
+    if historical_data:
+        post_content = format_for_linkedin(historical_data['events'])
+        print(f"length of content: {len(post_content)}")
+        print()
+        print(f"post content: {post_content}")
+
+        result = social_media_automator.create_post(post_content)
+    
+        print(result)
+
+    # Post on Discord
+    """
+    try:
+        message = jokes.get_programming_joke()
+        print(message)
+        result = discord_post_general(message)
+        print(f"result: {result}")
+    except Exception as e:
+        print(f"Failed to post: {str(e)}")
+        """
